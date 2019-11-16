173bebd18c2b8ccaeb632fbd4ac0170aad07137b
weizhenhua-1101 <cherishWZH@163.com>
2019-11-16 14:41:04 +0800

    初次提交哈哈哈

---
+++ demo1.java
@@ -0,0 +1,20 @@
+package DEMO;
+
+import java.util.Scanner;
+
+public class demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		String j;
+		Scanner reader = new Scanner(System.in);
+		System.out.println("请输入第一个数");
+		i = reader.nextInt();
+		System.out.println(i);
+		System.out.println("请输入一个字符串");
+		j = reader.next();
+		System.out.println(j);
+	}
+
+}
