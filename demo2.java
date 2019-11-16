4ef3349090169762b6f5891a1f18d5a200a2dfc8
weizhenhua-1101 <cherishWZH@163.com>
2019-11-16 15:02:45 +0800

    初次提交哈哈哈

---
+++ demo2.java
@@ -0,0 +1,13 @@
+package DEMO;
+
+public class demo2 {
+
+	public static void main(String[] args) {
+		int a = 1;
+		int b = 2;
+		System.out.println("Ìõ¼þÔËËã·û" + (a>b?a:b));
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.MIN_VALUE);
+	}
+
+}
